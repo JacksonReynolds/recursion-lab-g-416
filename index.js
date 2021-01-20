a14574aca8335747aa797eae40086a28400d3b5b
Jackson Reynolds <jwreynold@gmail.com>
2021-01-20 13:11:48 -0500

    commit tree eab2b164ea031f79cca3035b25b93d2ef6a87f35

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // Code your solution here!
 
+const printString = string => {
+    console.log(string[0])
+    if (string.length > 1) {
+        let substring = string.substring(1, string.length)
+        printString(substring)
+    } else {
+        return true
+    }
+}
+
+const reverseString = str => {
+    if (str === '') {
+        return ''
+    } else {
+        return reverseString(str.substring(1,str.length)) + str.charAt(0)
+    }
+}
+
+const isPalindrome = str => {
+    if (str.length <= 1) return true
+    let [firstLetter] = str
+    let lastLetter = str[str.length-1]
+    if (firstLetter === lastLetter) {
+        return isPalindrome(str.substring(1, str.length-1))
+    } else return false
+}
